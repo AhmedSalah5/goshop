dc3bfe568c6315c5f7ecd9a596018065178e0af6
AhmedSalah <ahmedmahdy5005@gmail.com>
2021-09-20 23:14:40 +0200

    Upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    //Open Plans Compare
+    $('.ec-plans-compare-btn').click(function(e){
+        e.preventDefault();
+        $('.ec-plans-compare').slideToggle(300);
+    })
+})
